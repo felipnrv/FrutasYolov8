2c403018446eacde5fef65891c24d73814f6c607
Felipe <felipe.98np@outlook.com>
2024-01-12 17:17:27 -0500

    Add object tracking and counting functionality

--- Conteo.py
+++ Conteo.py
@@ -1,21 +1,113 @@
-from ultralytics import YOLO
-from ultralytics.solutions import object_counter
 import cv2
+import pandas as pd
+from ultralytics import YOLO
+import cvzone
+import numpy as np
+from tracker import*
 
-model = YOLO("yolov8n.pt")
-cap = cv2.VideoCapture("D:\Felipe\Codigo\Model\cars.mp4")
-assert cap.isOpened(), "Error reading video file"
+# Load YOLO model
+model = YOLO('D:/Felipe/Codigo/Model/best.pt')
 
-counter = object_counter.ObjectCounter()  # Init Object Counter
-region_points = [(20, 400), (1080, 404), (1080, 360), (20, 360)]
-counter.set_args(view_img=True,
-                 reg_pts=region_points,
-                 classes_names=model.names,
-                 draw_tracks=True)
+def RGB(event, x, y, flags, param):
+    """
+    Mouse event callback function that prints the coordinates of the mouse cursor when it moves.
 
-while cap.isOpened():
-    success, im0 = cap.read()
-    if not success:
-        exit(0)
-    tracks = model.track(im0, persist=True, show=False)
-    im0 = counter.start_counting(im0, tracks)
+    Args:
+        event (int): The type of mouse event.
+        x (int): The x-coordinate of the mouse cursor.
+        y (int): The y-coordinate of the mouse cursor.
+        flags (int): Additional flags.
+        param (object): Additional parameters.
+
+    Returns:
+        None
+    """
+    if event == cv2.EVENT_MOUSEMOVE :  
+        point = [x, y]
+        print(point)
+
+# Create a named window and set the mouse callback function
+cv2.namedWindow('RGB')
+cv2.setMouseCallback('RGB', RGB)
+
+# Open video capture
+cap = cv2.VideoCapture(0)
+
+# Read class names from file
+my_file = open("config.yaml", "r")
+data = my_file.read()
+class_list = data.split("\n") 
+
+# Initialize counters and trackers
+count = 0
+tracker = Tracker()
+tracker1 = Tracker()
+tracker2 = Tracker()
+cy1 = 184
+cy2 = 209
+offset = 8
+upcar = {}
+downcar = {}
+countercarup = []
+countercardown = []
+downbus = {}
+counterbusdown = []
+upbus = {}
+counterbusup = []
+downtruck = {}
+countertruckdown = []
+
+while True:    
+    # Read frame from video capture
+    ret, frame = cap.read()
+    if not ret:
+        break
+    count += 1
+    if count % 3 != 0:
+        continue
+    frame = cv2.resize(frame, (1020, 500))
+
+    # Perform object detection using YOLO model
+    results = model.predict(frame)
+    a = results[0].boxes.data
+    px = pd.DataFrame(a).astype("float")
+    
+    list = []
+    list1 = []
+    list2 = []
+    for index, row in px.iterrows():
+        x1 = int(row[0])
+        y1 = int(row[1])
+        x2 = int(row[2])
+        y2 = int(row[3])
+        d = int(row[5])
+        c = class_list[d]
+        if 'Pitahaya' in c:
+            list.append([x1, y1, x2, y2])
+        elif 'Granadilla' in c:
+            list1.append([x1, y1, x2, y2])
+        elif 'Arandanos' in c:
+            list2.append([x1, y1, x2, y2])
+
+    # Update object trackers
+    bbox_idx = tracker.update(list)
+    for bbox in bbox_idx:
+        x3, y3, x4, y4, id1 = bbox
+        cx3 = int(x3 + x4) // 2
+        cy3 = int(y3 + y4) // 2
+        cv2.circle(frame, (cx3, cy3), 4, (255, 0, 0), -1)
+        cv2.rectangle(frame, (x3, y3), (x4, y4), (255, 0, 255), 2)
+        cvzone.putTextRect(frame, f'{id1}', (x3, y3), 1, 1)
+
+    # Draw counting lines
+    cv2.line(frame, (1, cy1), (1018, cy1), (0, 255, 0), 2)
+    cv2.line(frame, (3, cy2), (1016, cy2), (0, 0, 255), 2)
+
+    # Display frame
+    cv2.imshow("RGB", frame)
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
+
+# Release video capture and destroy windows
+cap.release()
+cv2.destroyAllWindows()
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from ultralytics.solutions import object_counter
 import cv2
 
 
+outfile = open('new3.0.txt', 'w')
 model = YOLO('D:/Felipe/Codigo/Model/best.pt')
 cap = cv2.VideoCapture(0)
 
@@ -14,7 +15,7 @@ points=[(0,300),(1080,300)]#puntos de la region de conteo, se puede cambiar por 
 counter.set_args(view_img=True,reg_pts=points,classes_names=model.names,draw_tracks=False)#configura el objeto contador
 
 
-while cap.isOpened():
+while True:
     ret,frame=cap.read()
 
     if not ret:
@@ -22,11 +23,13 @@ while cap.isOpened():
         exit(0)
     tracks=model.track(frame,persist=True,show=False) #persist es para que se mantenga el objeto en pantalla,show es para que se muestre la imagen
     counter.start_counting(frame,tracks) #tracks es una lista de objetos detectados, cada objeto tiene un id, un rectangulo y una clase
-
+    
     #Para cerrar la ventana se presiona la tecla k
-   
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    outfile.write(str(tracks)) 
+    if cv2.waitKey(1) & 0xFF == ord('q') or cv2.waitKey(1) & 0xFF == 27:
+        outfile.close()
         break
+    
 
 cap.release()
 cv2.destroyAllWindows()
