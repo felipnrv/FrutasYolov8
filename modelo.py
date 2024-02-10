69f94cf830a5cacdea2cc7b17a330b1a76a371f7
Felipe <felipe.98np@outlook.com>
2024-02-09 22:22:13 -0500

    agregue subpaginas y diseños

--- modelo.py
+++ modelo.py
@@ -2,10 +2,10 @@ import cv2
 from ultralytics import YOLO
 import supervision as sv
 import pyrebase
-from flask import Flask 
-from flask import render_template
-from flask import Response
+from flask import Flask,render_template,Response,request,redirect,url_for 
 import datetime as dt
+import pandas as pd
+import matplotlib.pyplot as plt
 
 
 app = Flask(__name__,template_folder='plantilla') #se inicializa la aplicacion
@@ -21,9 +21,10 @@ config = {
         "measurementId": "G-EMQTJ61ZL6"
     }
 
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
+auth = firebase.auth()
 
-
-#Global variables#--------------------------------------
 LINEA_INICIO = sv.Point(450, 0) #punto de inicio de la linea,la coordenada x es 320 y la coordenada y es 0
 LINEA_FINAL = sv.Point(450, 480) #punto final de la line, la coordenada x es 320 y la coordenada y es 480
 
@@ -34,8 +35,10 @@ fruta4_pitah = 3 # pitahaya
 fruta5_tomate = 4# tomate de arbol
 
 child_nombre = dt.datetime.now()
-child_nombre = child_nombre.strftime(format='%Y-%m-%d %H:%M:%S')
+child_nombre = child_nombre.strftime(format='%Y-%m-%d')
 #una clase es un objeto que tiene atributos y metodos y una funcion es un bloque de codigo que se ejecuta cuando se llama
+
+
 class linea_conteo_class():
     def __init__(self,id_clase,linea_conteo):
         self.id_clase = id_clase #es el numero de la clase que se quiere contar
@@ -46,17 +49,21 @@ class linea_conteo_class():
         deteccion = deteccion[deteccion.class_id == self.id_clase] #se filtran las detecciones de la clase que se quiere contar
 
         if result.boxes.id is not None:#se asigna el id del tracker a las detecciones
-            deteccion.tracker_id = result.boxes.id.cpu().numpy().astype(int) #se asigna el id del tracker a las detecciones,cpu para convertir a numpy y astype para convertir a int
+            deteccion.tracker_id = result.boxes.id.cpu().numpy().astype(int) 
             # se usa boxes.id.cpu().numpy() para obtener el id del tracker, es cpu porque se esta trabajando con un tensor de pytorch
 
         self.linea_conteo.trigger(detections=deteccion)#se cuentan las detecciones que cruzan la linea
-        conteo_in = self.linea_conteo.in_count#se obtiene el numero de detecciones que cruzaron la linea,in de izquierda a derecha
-        conteo_out = self.linea_conteo.out_count#se obtiene el numero de detecciones que cruzaron la linea, out de derecha a izquierda
+        conteo_in = self.linea_conteo.in_count#in de derecha a izquierda
+        conteo_out = self.linea_conteo.out_count#out de izquierda a derecha
 
         return conteo_in,conteo_out
 
+#@app.route('/')
 
 def main():
+
+    mango_out=0
+    maracuya_out=0
 
     linea = sv.LineZone(start=LINEA_INICIO, end=LINEA_FINAL) #se crea el contador de la linea
     linea_1 = sv.LineZone(start=LINEA_INICIO, end=LINEA_FINAL)
@@ -70,7 +77,7 @@ def main():
     
     caja_anotador = sv.BoxAnnotator(thickness=2,text_thickness=1,text_scale=0.5) #se crea el objeto que permite anotar las cajas
 
-    # Create class objects
+    #se crea un objeto de la clase linea_conteo_class
     granadilla = linea_conteo_class(fruta1_grand, linea_1)
     mango = linea_conteo_class(fruta2_mango, linea_2)
     maracuya = linea_conteo_class(fruta3_marac, linea_3)
@@ -79,12 +86,12 @@ def main():
 
     model = YOLO("D:/Felipe/Codigo/Model/frutasmdl.pt")
 
-
-    for result in model.track(source=1, show=False, stream=True, agnostic_nms=True):
+    stop_detection = False
+    for result in model.track(source=0, show=False, stream=True, agnostic_nms=True):
         
         frame = result.orig_img
 
-        #Detect al objects
+        
         detections = sv.Detections.from_yolov8(result)
 
         detections = detections[detections.class_id != 10]
@@ -109,7 +116,7 @@ def main():
         if result.boxes.id is not None: 
             detections5.tracker_id = result.boxes.id.cpu().numpy().astype(int)
     
-        #labels = create_labels(model,detections)
+        
         labels = [ #se crea una lista con las etiquetas de las detecciones
         f"# {class_id}{model.model.names[class_id]} {confidence:0.2f}"
         for _, confidence, class_id,tracker_id
@@ -139,20 +146,67 @@ def main():
         linea_5.trigger(detections=detections5)
         linea_anotador.annotate(frame=frame, line_counter=linea)
 
-       
-        print('Mango',mango_out)
-        print('Granadilla',granadilla_out)
-        print('Maracuya',maracuya_out)
-        print('Pitahaya',pitahaya_out)
-        print('Tomatearbol',tomatearbol_out)
-      
-        #cv2.imshow("yolov8", frame)
+        
+        y_offset = 50  # Espacio vertical entre cada línea de texto
 
-        if (cv2.waitKey(1) == 27):
-            break
+        mango_text = f"Mango: {mango_out}"
+        cv2.putText(frame, mango_text, (50, y_offset), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 0), 2)#color negro en rgb es (0,0,0)
+        y_offset += 30  # Incrementar el desplazamiento vertical
 
-        firebase = pyrebase.initialize_app(config)
-        db = firebase.database()
+        granadilla_text = f"Granadilla: {granadilla_out}"
+        cv2.putText(frame, granadilla_text, (50, y_offset), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 0), 2)#0.5 es el tamaño de la letra,1 es el grosor de la letra
+        y_offset += 30
+
+        maracuya_text = f"Maracuya: {maracuya_out}"
+        cv2.putText(frame, maracuya_text, (50, y_offset), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 0), 2)
+        y_offset += 30
+
+        pitahaya_text = f"Pitahaya: {pitahaya_out}"
+        cv2.putText(frame, pitahaya_text, (50, y_offset), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 0), 2)
+        y_offset += 30
+
+        tomatearbol_text = f"Tomate de arbol: {tomatearbol_out}"
+        cv2.putText(frame, tomatearbol_text, (50, y_offset), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 0), 2)#blanco en rgb es (255,255,255)
+        y_offset += 30
+
+        if mango_out != 0:
+            print('Mango', mango_out)
+        if granadilla_out != 0:
+            print('Granadilla', granadilla_out)
+        if maracuya_out != 0:
+            print('Maracuya', maracuya_out)
+        if pitahaya_out != 0:
+            print('Pitahaya', pitahaya_out)
+        if tomatearbol_out != 0:
+            print('Tomatearbol', tomatearbol_out)
+
+        fecha = pd.to_datetime("today")
+
+        data={"Mango": [mango_out],"Maracuya":[maracuya_out],
+              "Granadilla":[granadilla_out],
+              "Pitahaya":[pitahaya_out],
+              "Tomate de arbol":[tomatearbol_out]}
+        
+        df = pd.DataFrame(data)
+
+        fecha_actual = pd.to_datetime("today").strftime("%d-%m-%Y")
+
+            # Crear la tabla
+        fig, ax = plt.subplots(figsize=(8, 5))
+        ax.axis('off')
+
+            # Agregar título
+        ax.text(0.5, 1.05, "Informe del conteo de frutas - " + fecha_actual, ha='center', fontsize=14)
+
+            # Mostrar la tabla
+        ax.table(cellText=df.values, colLabels=df.columns, loc='center', cellLoc='left')
+
+        
+
+        plt.savefig('output1.png', dpi=300, bbox_inches='tight')
+
+
+        
 
        
         db.child(child_nombre).update(
@@ -162,18 +216,73 @@ def main():
             "Pitahaya_Entrada": pitahaya_out,
             "Tomatearbol_Entrada": tomatearbol_out})
         
+
         (flag, encodedImage) = cv2.imencode(".jpg", frame)
         if not flag:
                 continue
         yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' +
                 bytearray(encodedImage) + b'\r\n')
-   
         
+    return {"mango: ":mango_out,"maracuya: ":maracuya_out}
+    #return render_template('index.html',mango_out_list=mango_out_list)
 
 @app.route('/')#se crea una ruta
-def index():
+def main_page():
+    return render_template('main.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        # Obtener datos del formulario
+        nombre = request.form['nombre']
+        apellido = request.form['apellido']
+        email = request.form['email']
+        password = request.form['password']
+
+        # Enviar datos a la base de datos de Firebase
+        try:
+            db.child(child_nombre).child(nombre).update({
+                "Nombre": nombre,
+                "Apellido": apellido,
+                "Correo": email,
+                "Contraseña": password
+            })
+        except Exception as e:
+            # Manejar errores si hay algún problema al actualizar la base de datos
+            print("Error:", e)
+            return "Error al registrar. Por favor, inténtalo de nuevo más tarde."
+        
+        
+        user=auth.create_user_with_email_and_password(email, password)
+        	
+        # Redirigir a la página principal después del registro exitoso
+        return redirect(url_for('main_page'))
+
+    # Renderizar la plantilla de registro
+    return render_template('registro.html')
     
-    return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        # Firebase Authentication
+        try:
+            user = auth.sign_in_with_email_and_password(email, password)
+            return redirect(url_for('video'))
+        except Exception as e:
+            print("Error:", e)
+            return render_template('login.html', error=str(e))
+
+    return render_template('login.html')
+
+@app.route('/video')#se crea una ruta
+def video():
+    
+    return render_template('video.html')
+
 
 @app.route('/video_feed')
 def video_feed():
